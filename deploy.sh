92184935defdf58686299f83bda6de7c378afab0
Pierre Kuhner <pkuhner@users.noreply.github.com>
2017-11-22 20:35:20 +0100

    Fix secure env variables not being accessible in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-ssh $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH "git pull && git checkout -qf $TRAVIS_COMMIT"
-ssh $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH "yarn install && (echo $(date) > config/BUILD) && pm2 restart app"
+ssh $1@$2:$3 "git pull && git checkout -qf $TRAVIS_COMMIT"
+ssh $1@$2:$3 "yarn install && (echo $(date) > config/BUILD) && pm2 restart app"
 echo "Deployed $TRAVIS_COMMIT to production"
