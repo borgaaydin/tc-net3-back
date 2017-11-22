a37fb703bf8f3c3aff6d8c2641f3f745e447028b
Pierre Kuhner <pkuhner@users.noreply.github.com>
2017-11-22 20:28:15 +0100

    travis: Use SSH key auth

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,3 @@
-export SSHPASS=$DEPLOY_PASS
-sshpass -e ssh $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH "git pull && git checkout -qf $TRAVIS_COMMIT"
-sshpass -e ssh $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH "yarn install && (echo $(date) > config/BUILD) && pm2 restart app"
+ssh $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH "git pull && git checkout -qf $TRAVIS_COMMIT"
+ssh $DEPLOY_USER@$DEPLOY_HOST:$DEPLOY_PATH "yarn install && (echo $(date) > config/BUILD) && pm2 restart app"
 echo "Deployed $TRAVIS_COMMIT to production"
