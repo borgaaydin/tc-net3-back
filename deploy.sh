20a4d271a8f36486da7a23b2d5ad0daf7b1012c3
Pierre Kuhner <pkuhner@users.noreply.github.com>
2017-11-23 11:34:40 +0100

    Error handling in deploy script

--- deploy.sh
+++ deploy.sh
@@ -10,42 +10,63 @@ echo "Dumping current database..."
 docker exec $2 mongodump --archive=$3.gz --gzip --db $3
 if [ $? -eq 0 ]; then
     echo "OK"
+else
+    echo "FAIL"
+    exit 1
 fi
 
 echo "Copying dump on host..."
 docker cp $2:$3.gz $(git rev-parse HEAD).gz
 if [ $? -eq 0 ]; then
     echo "OK"
+else
+    echo "FAIL"
+    exit 1
 fi
 
 echo "Updating commit ID in last.build..."
 echo $(git rev-parse HEAD) > last.build
 if [ $? -eq 0 ]; then
     echo "OK"
+else
+    echo "FAIL"
+    exit 1
 fi
 
 echo "Pulling repository..."
 git pull origin master
 if [ $? -eq 0 ]; then
     echo "OK"
+else
+    echo "FAIL"
+    exit 1
 fi
 
 echo "Checking out travis build commit..."
 git checkout -qf $1
 if [ $? -eq 0 ]; then
     echo "OK"
+else
+    echo "FAIL"
+    exit 1
 fi
 
 echo "Running yarn install..."
 yarn install
 if [ $? -eq 0 ]; then
     echo "OK"
+else
+    echo "FAIL"
+    exit 1
 fi
 
 echo "Running yarn upgrade..."
 yarn upgrade
 if [ $? -eq 0 ]; then
     echo "OK"
+else
+    echo "FAIL"
+    exit 1
 fi
 
 echo "Updating build date..."
@@ -53,10 +74,16 @@ echo $(date) > config/BUILD
 echo $(date) > last.build.date
 if [ $? -eq 0 ]; then
     echo "OK"
+else
+    echo "FAIL"
+    exit 1
 fi
 
 echo "Restarting node server..."
 pm2 restart app
 if [ $? -eq 0 ]; then
     echo "OK"
+else
+    echo "FAIL"
+    exit 1
 fi
